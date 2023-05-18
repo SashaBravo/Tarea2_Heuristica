665541b6aed927490c0378f25332e54479889208
Matias Bustos <matias.bustos.salazar2001@gmail.com>
2023-05-17 22:21:34 -0400

    Mecanicas Hechas
    +Poner y quitar luces
    +Las luces iluminan hasta llegar a un muro
    +Condicion de Victoria
    -Falta la condicion de que no pueda ganar si hay mas luces en la misma fila o columna (y se alumbren mutuamente)

--- Akari.cpp
+++ Akari.cpp
@@ -6,8 +6,6 @@ const int COLUMNAS = 5;
 using namespace std;
 
 void print(GameState game){
-
-    uint64_t cells = CellsWLight(game);
     uint64_t one = firstBit;
     char a = 'A';
     cout<< "  ";
@@ -24,12 +22,10 @@ void print(GameState game){
         if((i+8)%8 == 0) { cout<< aux <<" "; }
 
         cout << BG_BLACK;
-        if(one&game.Cell_On){ cout<<BG_YELLOW; }
+        if(one&game.Cell_On){ cout<<BG_YELLOW<<YELLOW; }
         else { cout<<BG_WHITE<<WHITE; }
 
-        
-
-        if(one&cells){ cout<<BG_BLACK<<BLACK; }
+        if(one&game.Cells_inBlack){ cout<<BG_BLACK<<BLACK; }
 
         cout << "[";
 
@@ -41,13 +37,13 @@ void print(GameState game){
         if(one&game.Cell_4){ cout<<WHITE<<"4"<< RESET << BG_BLACK; }
         if(one&game.Cell_AnyBulb){ cout<<BLACK<<"."<< RESET << BG_BLACK; }
 
-        if((one&game.Cell_On)&~game.Lights){ cout<<YELLOW<<"."<< RESET << BG_BLACK; }
-        if((one^game.Cell_On)&~cells){cout<<WHITE<<"."<< RESET << BG_BLACK;};
+        if((one&(game.Cell_On&~game.Lights))&~game.Cells_inBlack){ cout<<YELLOW<<"1"<< RESET << BG_BLACK; }
+        if((one&~(game.Cell_On))&~game.Cells_inBlack){cout<<WHITE<<"2"<< RESET << BG_BLACK; }  
 
-        if(one&game.Cell_On){ cout<<BG_YELLOW; }
+        if(one&game.Cell_On){ cout<<BG_YELLOW<<YELLOW; }
         else { cout<<BG_WHITE; }
 
-        if(one&cells){ cout<<BG_BLACK<<BLACK; }
+        if(one&game.Cells_inBlack){ cout<<BG_BLACK<<BLACK; }
 
         cout << "]";
 
@@ -57,20 +53,25 @@ void print(GameState game){
     cout<<endl;
 }
 
-uint64_t CellsWLight(GameState game){
+uint64_t cellsWLight(GameState game){
 
     uint64_t cells = game.Cell_AnyBulb|game.Cell_0|game.Cell_1|game.Cell_2|game.Cell_3|game.Cell_4;
 
     return cells;
 } 
 
-void CreateBoard(GameState& game){
+void createBoard(GameState& game){
+
+    cout<<"Tablero Inicial"<<endl;
 
     srand((unsigned) time(NULL));
 
-    int aux = rand() % 2;
+    int auxRand = differentsBoards.size();
+
+    int aux = rand() % auxRand;
 
     string auxiliar = differentsBoards[aux];
+    
     int a = auxiliar.length();
     for(int i = 0; i < a; i++)
     {
@@ -81,16 +82,201 @@ void CreateBoard(GameState& game){
         else if(auxiliar[i] == '3'){ game.Cell_3 |= (firstBit>>i); }
         else if(auxiliar[i] == '4'){ game.Cell_4 |= (firstBit>>i); }
     }
+
+    game.Cells_inBlack = game.Cell_0|game.Cell_1|game.Cell_2|game.Cell_3|game.Cell_4|game.Cell_AnyBulb;
+
+    print(game);
+
+}
+
+void putLight(GameState& game){
+
+    string pos;
+    int row,col;
+    while(1){
+        cout<<"Ingrese un cordenada (5d) :";
+        cin>>pos;
+        cout<<endl;
+
+        row = pos[0]-49;
+        col = pos[1]-97;
+
+        if((0 <= row && row <= 7) && (0 <= col && col <= 8) && ((firstBit>>((row*8)+col))&~game.Cells_inBlack)){ 
+
+            if((firstBit>>((row*8)+col)) &~ game.Lights){
+                game.Lights |= firstBit>>((row*8)+col);
+                illumBoard(game, true);
+            }
+            else{
+                game.Lights ^= firstBit>>((row*8)+col);
+                illumBoard(game, false);
+            }
+            
+            if(victoryCondition(game)){
+                break;
+            }
+        }
+        else{ cout<<"Posicion Invalida"<<endl; }
+    }
+}
+
+void illumBoard(GameState& game, bool turnOn){
+    
+    if(turnOn){
+        auto one = firstBit;
+        for(int i=0;i<64;i++){
+            bool up = true;
+            bool down = true;
+            bool left = true;
+            bool right = true;
+
+            if(one&game.Lights){
+                for(int j = 0; j < 8; j++){
+                    if(((one>>j)&~game.Cells_inBlack) && right){
+                        game.Cell_On |= one>>j;
+                        if((one>>j)&lastCol) { right = false; }
+                    }
+                    else{ right = false; }
+
+                    if(((one<<j)&~game.Cells_inBlack) && left){
+                        game.Cell_On |= one<<j;
+                        if((one<<j)&firstCol) { left = false; }
+                    }
+                    else{ left = false; }
+
+                    if(((one>>(j*8))&~game.Cells_inBlack) && down){
+                        game.Cell_On |= (one>>(j*8));
+                        if((one>>(j*8))&lastRow) { down = false; }
+                    }
+                    else{ down = false; }
+
+                    if(((one<<(j*8))&~game.Cells_inBlack) && up){
+                        game.Cell_On |= (one<<(j*8));
+                        if((one<<(j*8))&firstRow) { up = false; }
+                    }
+                    else{ up = false; }
+                }
+            }
+            one >>= 1;
+        }
+    }
+    else{
+        auto one = firstBit;
+        for(int i=0;i<64;i++){
+            if((one&game.Cell_On)&~game.Cells_inBlack){
+                bool up = true;
+                bool down = true;
+                bool left = true;
+                bool right = true;
+                bool turnOff = true;
+
+                for(int j = 0; j < 8; j++){
+
+                    if(((one>>j)&~game.Cells_inBlack) && right){
+                        if((one>>j)&game.Lights) { turnOff = false; }
+                        if((one>>j)&lastCol) { right = false; }
+                    }
+                    else{ right = false; }
+
+                    if(((one<<j)&~game.Cells_inBlack) && left){
+                        if((one<<j)&game.Lights) { turnOff = false; }
+                        if((one<<j)&firstCol) { left = false; }
+                    }
+                    else{ left = false; }
+
+                    if(((one>>(j*8))&~game.Cells_inBlack) && down){
+                        if((one>>(j*8))&game.Lights) { turnOff = false; }
+                        if((one>>(j*8))&lastCol) { down = false; }
+                    }
+                    else{ down = false; }
+
+                    if(((one<<(j*8))&~game.Cells_inBlack) && up){
+                        if((one<<(j*8))&game.Lights) { turnOff = false; }
+                        if((one<<(j*8))&lastCol) { up = false; }
+                    }
+                    else{ up = false; }
+                }
+                if(turnOff){ game.Cell_On^= one; }
+            }
+            one >>= 1;
+        }
+    }
+
+    print(game);
+}
+
+bool victoryCondition(GameState game){
+    auto one = firstBit;
+
+    if((game.Cells_inBlack | game.Cell_On) == game.Board)
+    {
+        for(int i=0;i<64;i++){
+
+            if(one&game.Cell_0){
+                if((one>>1)&game.Lights){ return false; }
+                if((one<<1)&game.Lights){ return false; }
+                if((one>>8)&game.Lights){ return false; }
+                if((one<<8)&game.Lights){ return false; }
+
+                cout<<"casilla de 0 ok"<<endl;
+            }
+            if(one&game.Cell_1){
+                int count = 0;
+                if((one>>1)&game.Lights){ count++; }
+                if((one<<1)&game.Lights){ count++; }
+                if((one>>8)&game.Lights){ count++; }
+                if((one<<8)&game.Lights){ count++; }
+
+                if(count>1){ return false; }
+                else if(count<1){ return false; }
+                else{ cout<<"casilla de 1 ok"<<endl; }
+            }
+            if(one&game.Cell_2){
+                int count = 0;
+                if((one>>1)&game.Lights){ count++; }
+                if((one<<1)&game.Lights){ count++; }
+                if((one>>8)&game.Lights){ count++; }
+                if((one<<8)&game.Lights){ count++; }
+
+                if(count>2){ return false; }
+                else if(count<2){ return false; }
+                else{ cout<<"casilla de 2 ok"<<endl; }
+            }
+            if(one&game.Cell_3){
+                int count = 0;
+                if((one>>1)&game.Lights){ count++; }
+                if((one<<1)&game.Lights){ count++; }
+                if((one>>8)&game.Lights){ count++; }
+                if((one<<8)&game.Lights){ count++; }
+
+                if(count>3){ return false; }
+                else if(count<3){ return false; }
+                else{ cout<<"casilla de 3 ok"<<endl; }
+            }
+            if(one&game.Cell_4){
+                int count = 0;
+                if((one>>1)&game.Lights){ count++; }
+                if((one<<1)&game.Lights){ count++; }
+                if((one>>8)&game.Lights){ count++; }
+                if((one<<8)&game.Lights){ count++; }
+
+                if(count>4){ return false; }
+                else if(count<4){ return false; }
+                else if(count==4){ cout<<"casilla de 4 ok"<<endl; }
+            }
+            one >>= 1;
+        }
+        return true;
+    }
+
+    else{ return false;}
 }
 
 int main(){
     GameState game;
 
-    cout<<"Tablero Inicial"<<endl;
-
-    CreateBoard(game);
-
-    print(game);
+    createBoard(game);
+    putLight(game);
 
     return 0;
 
--- Akari.h
+++ Akari.h
@@ -34,19 +34,20 @@ uint64_t borde=0xff818181818181ff;
 uint64_t firstCol=0x8080808080808080;
 uint64_t lastCol=0x0101010101010101;
 
+//Tableros de la pagina /* krazydad.com/play/akari */
 vector<string> differentsBoards = {
-    ".1x..12..........xx..1x..................x1..x2..........xx..xx.", //  Tablero 1   Listo
-    "....xx...4....2.x.x..2..x..............x..3..x.x.3....3...xx....", //  Tablero 2   Listo
-    ".1x..12..........xx..1x..................x1..x2..........xx..xx.", //  Tablero 3   Cambiar
-    ".1x..12..........xx..1x..................x1..x2..........xx..xx.", //  Tablero 4   Cambiar
-    ".1x..12..........xx..1x..................x1..x2..........xx..xx.", //  Tablero 5   Cambiar
-    ".1x..12..........xx..1x..................x1..x2..........xx..xx.", //  Tablero 6   Cambiar
-    ".1x..12..........xx..1x..................x1..x2..........xx..xx.", //  Tablero 7   Cambiar
-    ".1x..12..........xx..1x..................x1..x2..........xx..xx."  //  Tablero 8   Cambiar
+    ".1x..12..........xx..1x..................x1..x2..........xx..xx.", //  Tablero 1   1-1-1
+    "....xx...4....2.x.x..2..x..............x..3..x.x.3....3...xx....", //  Tablero 2   1-1-2
+    ".xxx1xx............0x...x1....1x.2....1...................1xx2..", //  Tablero 3   2-50-16
+    "1.x1x..x...........x....x..x..1..2..2..x....x...........1..x1x.1", //  Tablero 4   2-50-17
+    "......x.1xx...0.....x.1...3..........3...2.x.....x...xx1.x......", //  Tablero 5   2-53-17
+    "..x..x....x.2...2.....3x.x............x.x3.....x...x.1....1..x..", //  Tablero 6   1-53-17
+    "...x.x2.x.2.....x.....x....1x..x2..11....x.....x.....x.2.1x.x...", //  Tablero 7   2-67-15
+    ".xxx...x....3...0....1.....x......x.2...1...x.3......4....0....."  //  Tablero 8   2-67-16
     };
 
 struct GameState{
-    uint64_t Board = 0x0;
+    uint64_t Board = full;
 
     uint64_t Lights = 0x0;
     uint64_t Cell_On = 0x0;
@@ -61,7 +62,12 @@ struct GameState{
     uint64_t Cell_4 = 0x0;
 };
 
-uint64_t CellsWLight(GameState game);
+uint64_t cellsWLight(GameState game);
 
 void print(GameState game, uint64_t board);
-void CreateBoard(GameState& game);
+void createBoard(GameState& game);
+
+void putLight(GameState& game);
+void illumBoard(GameState& game, bool turnOn);
+
+bool victoryCondition(GameState game);
